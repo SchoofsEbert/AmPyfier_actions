db0081b50551f9451f7db4e5fe5a8bc3e69cc62b
Ebert Schoofs <ebertschoofs@gmail.com>
2022-03-10 17:12:34 +0100

    Another try

--- dist/index.js
+++ dist/index.js
@@ -3018,8 +3018,8 @@ async function setup() {
 
 async function create_diff_file(start_commit, end_commit) {
     core.info("Creating diff file")
-    await exec.exec("ls")
-    await exec.exec('/bin/bash -c "./diff_scripts/git-diff-changed-lines.sh ' + start_commit + ' ' + end_commit + '  > ampyfier_diff"')
+    await exec.exec(`ls ${__dirname}`)
+    await exec.exec('/bin/bash -c "/diff_scripts/git-diff-changed-lines.sh ' + start_commit + ' ' + end_commit + '  > ampyfier_diff"')
 }
 
 async function run_ampyfier(project_dir, test, arguments) {
